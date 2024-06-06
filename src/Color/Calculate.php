c82a1213d5dbc235af657c5a3ff72cc6996f5265
Mathias Lipowski <dev@mixable.media>
2024-06-06 12:24:58 +0200

    Calculate::readableColorForLightAndDarkBackground now uses HSL values
    This results are much better color values

--- src/Color/Calculate.php
+++ src/Color/Calculate.php
@@ -40,23 +40,23 @@ class Calculate
     public static function readableColorForLightAndDarkBackground(string $hex): string
     {
         $rgb = Convert::hex2rgb($hex);
-        $hsv = Convert::rgb2hsv($rgb);
+        $hsl = Convert::rgb2hsl($rgb);
 
         $step = 0.01;
         $brightness = Calculate::weightedW3C($rgb);
         if ($brightness < 127) {
-            while ($brightness < 127 && $hsv[2] >= 0 && $hsv[2] <= 1) {
-                $hsv[2] += $step;
-                $brightness = Calculate::weightedW3C(Convert::hsv2rgb($hsv));
+            while ($brightness < 127 && $hsl[2] >= 0 && $hsl[2] <= 255) {
+                $hsl[2] += $step;
+                $brightness = Calculate::weightedW3C(Convert::hsl2rgb($hsl));
             }
         } else {
-            while ($brightness > 127 && $hsv[2] >= 0 && $hsv[2] <= 1) {
-                $hsv[2] -= $step;
-                $brightness = Calculate::weightedW3C(Convert::hsv2rgb($hsv));
+            while ($brightness > 127 && $hsl[2] >= 0 && $hsl[2] <= 255) {
+                $hsl[2] -= $step;
+                $brightness = Calculate::weightedW3C(Convert::hsl2rgb($hsl));
             }
         }
 
-        return Convert::rgb2hex(Convert::hsv2rgb($hsv));
+        return Convert::rgb2hex(Convert::hsl2rgb($hsl));
     }
 
     /**
