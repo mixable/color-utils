79b135113f5edc90f92f6a521e136504b195f20d
Mathias Lipowski <dev@mixable.media>
2024-06-06 13:51:41 +0200

    Separated HSV and HSL methods for readable color calculation

--- src/Color/Calculate.php
+++ src/Color/Calculate.php
@@ -26,8 +26,8 @@ class Calculate
     }
 
     /**
-     * Calculate a color that is readable on light and dark backgrounds.
-     * The color value is based on the given hex color. For the calculation, the equivalent HSL color value is used.
+     * Calculate a color that is readable on light and dark backgrounds by using the equivalent _HSL_ color value.
+     * The color value is based on the given hex color
      * This will use the weighted W3C formula as described at:
      * @see https://mixable.blog/black-or-white-text-on-a-colour-background/
      *
@@ -57,6 +57,40 @@ class Calculate
         }
 
         return Convert::rgb2hex(Convert::hsl2rgb($hsl));
+    }
+
+    /**
+     * Calculate a color that is readable on light and dark backgrounds by using the equivalent _HSV_ color value.
+     * The color value is based on the given hex color.
+     * This will use the weighted W3C formula as described at:
+     * @see https://mixable.blog/black-or-white-text-on-a-colour-background/
+     *
+     * A detailed description of the outputs of this calculation is available at:
+     * @see https://mixable.blog/adjust-text-color-to-be-readable-on-light-and-dark-backgrounds-of-user-interfaces/
+     *
+     * @param string $hex base color as hex string
+     * @return string adjusted color for light and dark backgrounds
+     */
+    public static function readableColorForLightAndDarkBackgroundHsv(string $hex): string
+    {
+        $rgb = Convert::hex2rgb($hex);
+        $hsv = Convert::rgb2hsv($rgb);
+
+        $step = 0.01;
+        $brightness = Calculate::weightedW3C($rgb);
+        if ($brightness < 127) {
+            while ($brightness < 127 && $hsv[2] >= 0 && $hsv[2] <= 1) {
+                $hsv[2] += $step;
+                $brightness = Calculate::weightedW3C(Convert::hsv2rgb($hsv));
+            }
+        } else {
+            while ($brightness > 127 && $hsv[2] >= 0 && $hsv[2] <= 1) {
+                $hsv[2] -= $step;
+                $brightness = Calculate::weightedW3C(Convert::hsv2rgb($hsv));
+            }
+        }
+
+        return Convert::rgb2hex(Convert::hsv2rgb($hsv));
     }
 
     /**
--- tests/Color/CalculateTest.php
+++ tests/Color/CalculateTest.php
@@ -27,8 +27,22 @@ class CalculateTest extends \PHPUnit\Framework\TestCase
      */
     public function testReadableColorForLightAndDarkBackground()
     {
+        $this->assertEquals('fc5800', Calculate::readableColorForLightAndDarkBackground('#632300'));
+        $this->assertEquals('808080', Calculate::readableColorForLightAndDarkBackground('#454545'));
         $this->assertEquals('be721b', Calculate::readableColorForLightAndDarkBackground('#f0c696'));
         $this->assertEquals('00b5b5', Calculate::readableColorForLightAndDarkBackground('#73feff'));
         $this->assertEquals('8f8f00', Calculate::readableColorForLightAndDarkBackground('#ffff00'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testReadableColorForLightAndDarkBackgroundHsv()
+    {
+        $this->assertEquals('fc5900', Calculate::readableColorForLightAndDarkBackgroundHsv('#632300'));
+        $this->assertEquals('7f7f7f', Calculate::readableColorForLightAndDarkBackgroundHsv('#454545'));
+        $this->assertEquals('947a5c', Calculate::readableColorForLightAndDarkBackgroundHsv('#f0c696'));
+        $this->assertEquals('439596', Calculate::readableColorForLightAndDarkBackgroundHsv('#73feff'));
+        $this->assertEquals('8e8e00', Calculate::readableColorForLightAndDarkBackgroundHsv('#ffff00'));
     }
 }
